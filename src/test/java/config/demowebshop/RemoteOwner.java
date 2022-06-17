6bf11717ece164c219ce424ff929d43fbf90fc9e
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-06-17 16:38:56 +0300

    правки

--- src/test/java/config/demowebshop/RemoteOwner.java
+++ src/test/java/config/demowebshop/RemoteOwner.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:properties/remote.properties"
+        "classpath:remote.properties"
 })
 public interface RemoteOwner extends Config{
 
